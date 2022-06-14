de09afa1d3728a7bcaca87eaac099da3268dc449
linuxlite <eze.rey92@gmail.com>
2022-06-13 21:47:39 -0300

    mañana tengo que continuar con la busqueda de productos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import { React, useRef } from "react";
 
 export default function Header({ links }) {
+  const searchRef = useRef("");
+
+  const handdleClickSearch = ({ target }) => {
+    const { current: busquedaInput } = searchRef;
+    console.log("Buqueda...", busquedaInput.value);
+  };
   return (
     <header className="header">
       <ul className="ul">
@@ -16,8 +22,16 @@ export default function Header({ links }) {
         })}
       </ul>
 
-      <input className="inputSearch" type="search" placeholder="Search..." />
-      <button className="buttonSearch">Search</button>
+      <input
+        ref={searchRef}
+        className="inputSearch"
+        type="search"
+        placeholder="Search..."
+      />
+
+      <button onClick={(ev) => handdleClickSearch(ev)} className="buttonSearch">
+        Search
+      </button>
     </header>
   );
 }
